6cc18e31aa2ccb75d7710ba5e4831df0cc1d5e9f
Marina <Mari.rubel@gmail.com>
2018-03-18 16:41:48 +0200

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,25 @@
+package com.mainacad;
+
+import static java.lang.System.out;
+
+public class Exam {
+    public static void main(String[] args) {
+
+        //Car myCar = new Car();
+         String s = "Hello";
+         System.out.println(s + 3);
+
+
+    }
+}
+//Задача6
+/*
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i++ < 4)
+        {
+            System.out.println("Hello world!");
+        }
+    }
+*/
